6d237a5c31384170dc497ad9485869a9f6903846
weiwenjiang <jiang.wwen@gmail.com>
2020-03-29 02:18:06 +0800

    resutls

--- Interface/run.sh
+++ Interface/run.sh
@@ -4,7 +4,8 @@
 #for model in 'alexnet' 'densenet121' 'densenet161'  'densenet169' 'densenet201' 'squeezenet1_0' 'squeezenet1_1'  'vgg11_bn' 'vgg13' 'vgg13_bn' 'vgg16' 'vgg16_bn' 'vgg19' 'vgg19_bn' 'wide_resnet101_2' 'wide_resnet50_2'  'vgg11'  'resnet50' 'resnet101' 'resnet152' 'resnet18' 'resnet34';
 for model in 'mnasnet0_5' 'mobilenet_v2' 'mnasnet1_0' 'shufflenet_v2_x0_5' 'shufflenet_v2_x1_0';
 do
-	python explore_conv_dconv.py -m $model >> conv_dconv_dse_res &
+	touch res/$model
+	python explore_conv_dconv.py -m $model >> res/$model &
 done
 
 wait
