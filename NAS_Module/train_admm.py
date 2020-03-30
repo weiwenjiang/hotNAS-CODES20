fbf37dffa741d2ea76a8309f5ce729f5ed1832b8
weiwenjiang <jiang.wwen@gmail.com>
2020-03-30 18:19:38 -0400

    admm + pattern

--- NAS_Module/train_admm.py
+++ NAS_Module/train_admm.py
@@ -24,7 +24,7 @@ except ImportError:
     amp = None
 
 
-def train_one_epoch(model, criterion, optimizer, data_loader, device, epoch, print_freq, layer_names,
+def train_one_epoch(model, criterion, optimizer, admm_optimizer, data_loader, device, epoch, print_freq, layer_names,
                     percent, pattern, data_loader_test, arg_rho, apex=False):
 
     Z, U = utils.initialize_Z_and_U(model,layer_names)
@@ -44,40 +44,65 @@ def train_one_epoch(model, criterion, optimizer, data_loader, device, epoch, pri
         start_time = time.time()
         image, target = image.to(device), target.to(device)
         output = model(image)
-        # loss = criterion(output, target)
-
-        loss = utils.admm_loss(device, model, layer_names, criterion, Z, U, output, target, rho)
-
-        optimizer.zero_grad()
-        if apex:
-            with amp.scale_loss(loss, optimizer) as scaled_loss:
-                scaled_loss.backward()
-        else:
-            loss.backward()
-        optimizer.step()
-
-        acc1, acc5 = utils.accuracy(output, target, topk=(1, 5))
-        batch_size = image.shape[0]
-        metric_logger.update(loss=loss.item(), lr=optimizer.param_groups[0]["lr"])
-        metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
-        metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
-        metric_logger.meters['img/s'].update(batch_size / (time.time() - start_time))
-
-        batch_idx+=1
 
 
+        if batch_idx <= 2400:
+            loss = utils.admm_loss(device, model, layer_names, criterion, Z, U, output, target, rho)
 
-        if batch_idx%100 == 0:
-            print("=" * 10, "Entering ADMM Optimization")
-            X = utils.update_X(model, layer_names)
-            Z, layer_pattern = utils.update_Z_Pattern(X, U, layer_names, pattern)
-            U = utils.update_U(U, X, Z, layer_names)
-            rho = rho*10
+            admm_optimizer.zero_grad()
+            if apex:
+                with amp.scale_loss(loss, admm_optimizer) as scaled_loss:
+                    scaled_loss.backward()
+            else:
+                loss.backward()
+            admm_optimizer.step()
 
-        if batch_idx%500==0:
+            acc1, acc5 = utils.accuracy(output, target, topk=(1, 5))
+            batch_size = image.shape[0]
+            metric_logger.update(loss=loss.item(), lr=optimizer.param_groups[0]["lr"])
+            metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
+            metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
+            metric_logger.meters['img/s'].update(batch_size / (time.time() - start_time))
+
+            batch_idx+=1
+
+
+
+            if batch_idx%600 == 0:
+                print("=" * 10, "Entering ADMM Optimization")
+                X = utils.update_X(model, layer_names)
+                Z, layer_pattern = utils.update_Z_Pattern(X, U, layer_names, pattern)
+                U = utils.update_U(U, X, Z, layer_names)
+                rho = rho*10
+
+        if batch_idx==2400:
             if data_loader_test:
                 evaluate(model, criterion, data_loader_test, device=device, layer_names=layer_names, pattern= pattern)
-            # Plot([float(x) for x in list(X[layer_names[-1]].flatten())], plot_type=2)
+
+        if batch_idx>2400:
+            loss = criterion(output, target)
+
+            optimizer.zero_grad()
+            if apex:
+                with amp.scale_loss(loss, optimizer) as scaled_loss:
+                    scaled_loss.backward()
+            else:
+                loss.backward()
+            optimizer.step()
+
+            acc1, acc5 = utils.accuracy(output, target, topk=(1, 5))
+            batch_size = image.shape[0]
+            metric_logger.update(loss=loss.item(), lr=optimizer.param_groups[0]["lr"])
+            metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
+            metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
+            metric_logger.meters['img/s'].update(batch_size / (time.time() - start_time))
+
+            batch_idx += 1
+
+            if batch_idx%100 == 0:
+                if data_loader_test:
+                    evaluate(model, criterion, data_loader_test, device=device, layer_names=layer_names,
+                             pattern=pattern)
 
     return layer_pattern
 
