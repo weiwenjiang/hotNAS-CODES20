78446c685b22ddc97f9378f8914e127a905f880f
weiwenjiang <jiang.wwen@gmail.com>
2020-03-31 15:43:59 -0400

    DSE

--- Interface/search_space.py
+++ Interface/search_space.py
@@ -1,6 +1,6 @@
 search_space = {
     #                  Tm                    Tr   Tc       W_p              I_p                 O_p
-    'hw_only_cconv': (list(range(10,250,30)),[7,14,20,32],[7,14,20,32],list(range(2,10,2)),list(range(2,10,2)),list(range(2,10,2))),
+    'hw_only_cconv': (list(range(10,250,30)),[32,64,256],[32,64,256],list(range(2,10,2)),list(range(2,10,2)),list(range(2,10,2))),
     'hw_only_cconv2': (list(range(10,20,30)),[7,14],[7,14],list(range(2,3,2)),list(range(2,3,2)),list(range(2,3,2))),
     "hw_cd_cconv": (list(range(10,250,30)),[32],[32],list(range(2,10,2)),list(range(2,10,2)),list(range(2,10,2))),
     "hw_cd_dconv": (list(range(64,1024,128)),[32],[32]),
