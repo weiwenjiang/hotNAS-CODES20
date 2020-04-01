039b62503821004d14e92da8fdb1b214327363af
weiwenjiang <jiang.wwen@gmail.com>
2020-04-01 00:20:54 -0400

    ResNet 12s -> 11s test 100batch

--- NAS_Module/train_admm.py
+++ NAS_Module/train_admm.py
@@ -30,7 +30,7 @@ def re_train_one_epoch(model, criterion, optimizer, data_loader, device, epoch, 
 
     # Plot([float(x) for x in list(Z[layer_names[-1]].flatten())], plot_type=2)
 
-    start_time = time.time()
+    re_train_start_time = time.time()
 
 
     model.train()
@@ -67,13 +67,13 @@ def re_train_one_epoch(model, criterion, optimizer, data_loader, device, epoch, 
 
         batch_idx += 1
         if batch_idx%100==0:
-            total_time = time.time() - start_time
+            total_time = time.time() - re_train_start_time
             total_time_str = str(datetime.timedelta(seconds=int(total_time)))
             print("Elapsed Time {}".format(total_time_str) )
 
             evaluate(model, criterion, data_loader_test, device=device)
         elif batch_idx%1000==0:
-            total_time = time.time() - start_time
+            total_time = time.time() - re_train_start_time
             total_time_str = str(datetime.timedelta(seconds=int(total_time)))
             print("Elapsed Time {}".format(total_time_str))
             evaluate(model, criterion, data_loader_test, device=device)
