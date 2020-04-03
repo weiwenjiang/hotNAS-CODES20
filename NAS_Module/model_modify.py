70907894bc24f1200f79b7a0c378ebaccae9a702
weiwenjiang <jiang.wwen@gmail.com>
2020-04-03 03:47:07 -0400

    rl hw-sw co-explore

--- NAS_Module/model_modify.py
+++ NAS_Module/model_modify.py
@@ -122,8 +122,6 @@ def resnet_18_space(model, pattern_idx, k_expand, ch_list, q_list, args):
     Kenel_Expand(model, layer_kernel_inc)
     Kenel_Quantization(model, quant_layers, quan_paras)
 
-    print(model)
-
     return model
 
 
