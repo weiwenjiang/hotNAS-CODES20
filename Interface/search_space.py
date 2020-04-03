66cda9ec3abcfa0f4b232a549d51b3ed0f1a321d
weiwenjiang <jiang.wwen@gmail.com>
2020-04-03 02:32:29 -0400

    proxyless

--- Interface/search_space.py
+++ Interface/search_space.py
@@ -2,8 +2,8 @@ search_space = {
     #                  Tm                    Tr   Tc       W_p              I_p                 O_p
     'hw_only_cconv': (list(range(10,250,30)),[32,64,256],[32,64,256],list(range(2,21,4)),list(range(2,21,4)),list(range(2,21,4))),
     'hw_only_cconv2': (list(range(10,20,30)),[7,14],[7,14],list(range(2,3,2)),list(range(2,3,2)),list(range(2,3,2))),
-    "hw_cd_cconv": (list(range(10,250,30)),[32],[32],list(range(2,21,4)),list(range(2,21,4)),list(range(2,21,4))),
-    "hw_cd_dconv": (list(range(64,1024,128)),[32],[32]),
+    "hw_cd_cconv2": (list(range(10,250,30)),[32],[32],list(range(2,21,4)),list(range(2,21,4)),list(range(2,21,4))),
+    "hw_cd_dconv2": (list(range(64,1024,128)),[32],[32]),
     "hw_cd_cconv2": (list(range(10,20,30)),[14],[7,14],list(range(2,3,2)),list(range(2,3,2)),list(range(2,3,2))),
     "hw_cd_dconv2": (list(range(64,74,64)),[14],[7,14])
 
