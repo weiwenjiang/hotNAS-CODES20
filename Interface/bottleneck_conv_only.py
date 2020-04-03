6cfb740265fe0d06dda571de86e9b47f62c049da
weiwenjiang <jiang.wwen@gmail.com>
2020-04-03 03:49:41 -0400

    rl hw-sw co-explore

--- Interface/bottleneck_conv_only.py
+++ Interface/bottleneck_conv_only.py
@@ -45,7 +45,7 @@ def get_performance(model, Tm, Tn, Tr, Tc, Tk, W_p, I_p, O_p,device=None):
                                                                    HW_constraints["r_BRAM_Size"], HW_constraints["r_BRAM"],
                                                                    HW_constraints["BITWIDTH"])
 
-                print("\t",layer_name,M, N, R, C, K, S, T)
+                # print("\t",layer_name,M, N, R, C, K, S, T)
                 Layer = PM_Layer.Layer_Class(B, M, N, R, C, K, S, "cconv", P)
                 acc_1 = PM_FPGA_Template.FPGA_Templates(Tm, Tn, Tr, Tc,
                                                         Tk, W_p, I_p, O_p, "cconv", r_Ports, r_DSP, r_BRAM, r_BRAM_Size,
@@ -58,7 +58,7 @@ def get_performance(model, Tm, Tn, Tr, Tc, Tk, W_p, I_p, O_p,device=None):
                     else:
                         perf = acc_1.get_layer_latency(Layer)
                     cTT += perf[0]
-                    print(layer_name,perf[0]/10**5,perf[1],[x/10**5 for x in perf[2]])
+                    # print(layer_name,perf[0]/10**5,perf[1],[x/10**5 for x in perf[2]])
 
         elif isinstance(layer, nn.MaxPool2d) or isinstance(layer, nn.AdaptiveAvgPool2d) or isinstance(layer,
                                                                                                       nn.AvgPool2d):
