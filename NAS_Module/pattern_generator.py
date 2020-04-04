d423d75849b1f3235aaee960f402d9c879180eb0
weiwenjiang <jiang.wwen@gmail.com>
2020-04-04 18:53:57 -0400

    mobilenet 5*5 KP

--- NAS_Module/pattern_generator.py
+++ NAS_Module/pattern_generator.py
@@ -28,7 +28,7 @@ def pattern_sets_generate_3(kernal_shape):
 
         for i in range(4):
             tmp_tensor = base_tensor.clone()
-            tozero_idx = random.sample(range(25), 10)
+            tozero_idx = random.sample(range(25), 6)
             for idx in tozero_idx:
                 tmp_tensor[idx] = 0
             pattern_space[i] = tmp_tensor
