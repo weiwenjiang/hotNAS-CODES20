b5dec485b78341c67480d0888fa4c75dd4e96b77
weiwenjiang <jiang.wwen@gmail.com>
2020-04-09 02:57:02 -0400

    random

--- NAS_Module/train.py
+++ NAS_Module/train.py
@@ -36,6 +36,7 @@ except ImportError:
 best_acc5 = 0
 
 def save_chk_point(model_without_ddp,optimizer,lr_scheduler,epoch,acc5):
+    global args
     global best_acc5
     if acc5>best_acc5:
         best_acc5 = acc5
